8df6099a4378cab1e5d1bda84d03ed32392932e3
Steve Gentile <Steven.gentile@gmail.com>
2012-06-16 10:33:41 -0400

    adding initial sample

---
+++ js/default.js
@@ -0,0 +1,51 @@
+﻿/// <reference path="knockoutjs.js" />
+
+// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    WinJS.strictProcessing();
+
+    function appViewModel() {
+        this.self = this;
+        this.firstName = ko.observable("Steve");
+        this.lastName = ko.observable("Gentile");
+        this.fullName = ko.computed(function () {
+            return this.firstName() + " " + this.lastName();
+        }, this.self);
+    }
+
+    function initialize() {
+        ko.applyBindings(new appViewModel());
+    }
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
+
+    //If Document fully loaded than begin processing
+    document.addEventListener("DOMContentLoaded", initialize, false);
+})();
